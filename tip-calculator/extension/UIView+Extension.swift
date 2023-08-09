5795457d45df311b4ac3cd217b20e36a0d7aa215
soroush <sorjw71@gmail.com>
2023-08-09 19:04:16 +0330

    UIView's extension addRoundedCorners function implemented - SplitInputView subviews added

--- tip-calculator/extension/UIView+Extension.swift
+++ tip-calculator/extension/UIView+Extension.swift
@@ -25,4 +25,11 @@ extension UIView {
         layer.masksToBounds = false
         layer.cornerRadius = radius
     }
+    
+    func addRoundedCorners(corners: CACornerMask, radius: CGFloat) {
+        layer.cornerRadius = radius
+        // problem
+        layer.maskedCorners = [corners]
+    }
+    
 }
--- tip-calculator/view/SplitInputView.swift
+++ tip-calculator/view/SplitInputView.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class SplitInputView: UIView {
     
+    private let headerView: HeaderView = {
+        let view = HeaderView()
+        view.configure(topText: "Split", bottomText: "the total")
+        return view
+    }()
+    
+    private lazy var decrementButton: UIButton = {
+        let button = buildButton(
+            text: "-",
+            // problem
+            corners: [.layerMinXMaxYCorner, .layerMinXMinYCorner])
+        return button
+    }()
+    
+    private lazy var incrementButton: UIButton = {
+        let button = buildButton(
+            text: "+",
+            // problem
+            corners: [.layerMaxXMinYCorner, .layerMaxXMaxYCorner])
+        return button
+    }()
+    
+    private lazy var quantityLabel: UILabel = {
+        
+        let label = LabelFactory.build(
+            text: "1",
+            font: ThemeFont.bold(ofSize: 20),
+            backgroundColor: .white)
+        
+        return label
+    }()
+    
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            decrementButton,
+            quantityLabel,
+            incrementButton
+        ])
+        stackView.axis = .horizontal
+        stackView.spacing = 0
+        return stackView
+    }()
+    
+    
+    
     init() {
         // .zero: because we use autolayout, so we dont need to care about frame
         super.init(frame: .zero)
@@ -23,7 +69,36 @@ class SplitInputView: UIView {
     
     
     private func layout() {
-        backgroundColor = .systemBlue
+
+        [headerView, stackView].forEach(addSubview(_:))
+        
+        stackView.snp.makeConstraints { make in
+            make.top.bottom.trailing.equalToSuperview()
+        }
+        
+        // add width constraints for buttons
+        [incrementButton, decrementButton].forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height)
+            }
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalTo(stackView.snp.leading).offset(-24)
+            make.centerY.equalTo(stackView.snp.centerY)
+            make.width.equalTo(68)
+        }
+
+    }
+    
+    private func buildButton(text: String, corners: CACornerMask) -> UIButton {
+        let button = UIButton()
+        button.setTitle(text, for: .normal)
+        button.titleLabel?.font = ThemeFont.bold(ofSize: 20)
+        button.addRoundedCorners(corners: corners, radius: 8.0)
+        button.backgroundColor = ThemeColor.primary
+        return button
     }
     
 }
